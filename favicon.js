9dd302dc9ace6f18523de95632c4911cdf6a9cc3
Will B Chang <will.b.chang@gmail.com>
2021-01-26 14:35:42 +0800

    docs: add todo in comments

---
+++ favicon.js
@@ -0,0 +1,13 @@
+/*
+ * | Websites             | Favicon href values                                            |
+ * |----------------------+----------------------------------------------------------------|
+ * | https://github.com   | https://github.githubassets.com/favicons/favicon.png           |
+ * | https://jquery.com   | //jquery.com/jquery-wp-content/themes/jquery.com/i/favicon.ico |
+ * | https://pixabay.com  | /favicon-32x32.png                                             |
+ * | https://willbc.cn    | assets/images/favicon.png                                      |
+ */
+
+// 1. Get favicons in website
+// 2. Sort Favicons by name and quality.
+// 3. Convert Favicons to Base64
+// Test
