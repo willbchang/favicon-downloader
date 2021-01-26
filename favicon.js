4380ee4f835c037fe6c52094fbae11401f7b8271
Will B Chang <will.b.chang@gmail.com>
2021-01-26 22:07:44 +0800

    refactor: check url with regex

--- favicon.js
+++ favicon.js
@@ -12,17 +12,17 @@ function getFavicons() {
     const links = document.querySelectorAll('link[rel*=icon]')
     const favicons = [...links].map(link => {
         const href = link.href
-        try {
-            // Check if favicon's href is a valid URL
-            new URL(href)
-            return href
-        } catch (_) {
-            const origin = new URL(document.URL).origin
+        const origin = new URL(document.URL).origin
 
-            // assets/images/favicon.png
-            return origin + '/' + href
-        }
+        if (isURL(href)) return href
+        return origin + '/' + href
     })
+
+    return favicons
+
+    function isURL(url) {
+        return /^https?:\/\//.test(url)
+    }
 }
 console.log(getFavicons());
 // 2. Sort Favicons by name and quality.
