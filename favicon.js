1d75354f581c23d3759db0848370f866f36d72cf
Will B Chang <will.b.chang@gmail.com>
2021-01-28 12:02:53 +0800

    docs: update comments

--- favicon.js
+++ favicon.js
@@ -9,6 +9,7 @@
 
 // 0. Get HTML from url
 // Alternative, only if you want to use it in browser extension or server.
+// It may not work in the browser console due to cors
 export async function getHTML(url) {
     const response = await fetch(url)
     const text = await response.text()
@@ -89,7 +90,7 @@ export async function getDataURL(faviconURL) {
 
 export const fallbackSVG = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#4a5568"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>`
 
-// Main function
+// Example
 export async function getFavicon(url) {
     try {
         const html = await getHTML(url)
@@ -120,9 +121,9 @@ const urls = [
         "https://pixabay.com",
         "https://willbc.cn",
         "not a url",
-        "https://unavaliable.com",
+        "https://unavaliable.com", // This URL takes time to get result.
         "https://stackoverflow.com/questions/61212"
 ]
 
-// Uncomment this to test
+// Uncomment the code below to test
 //test(urls)
